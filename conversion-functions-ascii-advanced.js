07c844e64312c08048464f40ffe2415f9ec47b56
yoxiaoxiao <xiaoxiaofu2866@gmail.com>
2024-09-10 13:29:39 -0700

    finished

--- conversion-functions-ascii-advanced.js
+++ conversion-functions-ascii-advanced.js
@@ -9,32 +9,36 @@ Consult documentation on how to utilize the following methods:
 
 /* Base 2 to base 16 */
 function binaryToHexadecimal(blob) {
-  // Your code here 
+  return "0x" + parseInt(blob.slice(2), 2).toString(16);
 }
 
 /* Base 16 to base 2 */
 function hexadecimalToBinary(blob) {
-  // Your code here 
+  return `0b${parseInt(blob.slice(2), 16).toString(2)}`
 }
 
 /* Base 10 to ASCII */
 function decimalToAscii(blob) {
-  // Your code here 
+  return String.fromCharCode(blob);
 }
 
 /* Base 2 to ASCII */
 function binaryToAscii(blob) {
-  // Your code here 
+  return String.fromCharCode(blob);
 }
 
 /* Base 16 to ASCII */
 function hexadecimalToAscii(blob) {
-  // Your code here 
+  return String.fromCharCode(blob);
 }
 
 /* ASCII to base 10 */
 function asciiToDecimal(blob) {
-  // Your code here 
+  let result = [];
+  for (let i = 0; i < blob.length; i++) {
+    result.push(blob.charCodeAt(i));
+  }
+  return result;
 }
 
 // console.log('Binary to hexadecimal:')
